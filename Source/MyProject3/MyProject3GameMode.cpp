f1cdad7a9669c86a146ef4638a33e66e4f037131
Kevin Nygaard <balthamos89@gmail.com>
2015-05-02 16:07:29 -0500

    Initial commit
    - Adds base 3rd player map template
    - Adds door that opens/closes when you get near/far

---
+++ Source/MyProject3/MyProject3GameMode.cpp
@@ -0,0 +1,16 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+
+#include "MyProject3.h"
+#include "MyProject3GameMode.h"
+#include "MyProject3Character.h"
+
+AMyProject3GameMode::AMyProject3GameMode(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
