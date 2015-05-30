8219be36cfc3486ac0447c59dca0ce24616dcaae
Kevin Nygaard <balthamos89@gmail.com>
2015-05-30 09:32:19 -0500

    Adds first pass Item class
    - Changes CharacterBP to call into new C++ Item class
    - Adds crude static mesh for ID card
    - Adds BP Item Subclass for ID Card (deriving from new C++ Item class)
    - Currently, using the ID card will cause it to print a message (in the message
      log) and destroy the actor
    - The item is not given to the character, because the character is a Blueprint
      class, and you cannot cast to a Blueprint class inside C++
    - The Character class needs to be converted to C++ for us to continue further,
      but this at least shows proof of concept of using C++

---
+++ Source/MyProject3/Item.cpp
@@ -0,0 +1,70 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MyProject3.h"
+#include "Item.h"
+#include "Engine.h"
+#include "Net/UnrealNetwork.h"
+
+// Sets default values
+AItem::AItem(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	bReplicates = true;
+};
+
+void AItem::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+    DOREPLIFETIME(AItem, Name);
+    DOREPLIFETIME(AItem, bShowName);
+    DOREPLIFETIME(AItem, bIsStackable);
+    DOREPLIFETIME(AItem, MaxStackSize);
+    DOREPLIFETIME(AItem, InventoryImage);
+}
+
+
+// Called when the game starts or when spawned
+void AItem::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AItem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Test to see if item can be used
+bool AItem::CanUse(ACharacter* Character, UActorComponent* Component)
+{
+    return true;
+}
+
+// Attempt to use item
+void AItem::Use_Implementation(ACharacter* Character, UActorComponent* Component)
+{
+    UE_LOG(LogTemp, Log, TEXT("Hello"));//, *GetNameSafe(this), *GetNameSafe(Impact.GetActor()));
+    Destroy();
+}
+
+// Validate inputs
+bool AItem::Use_Validate(ACharacter* Character, UActorComponent* Component)
+{
+	return true;
+}
+
+// Character is looking at item
+bool AItem::StartFocus()
+{
+	return false;
+}
+
+// Character is not looking at item
+bool AItem::EndFocus()
+{
+	return false;
+}
---
+++ Source/MyProject3/Item.h
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Engine/StaticMeshActor.h"
+#include "Engine/Texture2D.h"
+#include "Components/ActorComponent.h"
+#include "Item.generated.h"
+
+UCLASS(Abstract, Blueprintable)
+class MYPROJECT3_API AItem : public AStaticMeshActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AItem(const FObjectInitializer& ObjectInitializer);
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick(float DeltaSeconds) override;
+
+	// Test to see if item can be used
+	UFUNCTION(BlueprintCallable, Category = Pickup)
+	bool CanUse(ACharacter* Character, UActorComponent* Component);
+
+	// Attempt to use item
+	UFUNCTION(Server, NetMulticast, Reliable, WithValidation, BlueprintCallable, Category = Pickup)
+	void Use(ACharacter* Character, UActorComponent* Component);
+	virtual void Use_Implementation(ACharacter* Character, UActorComponent* Component);
+	bool Use_Validate(ACharacter* Character, UActorComponent* Component);
+
+	// Character is looking at item
+	UFUNCTION(BlueprintCosmetic, BlueprintCallable, Category = Pickup)
+	bool StartFocus();
+
+	// Character is not looking at item
+	UFUNCTION(BlueprintCosmetic, BlueprintCallable, Category = Pickup)
+	bool EndFocus();
+
+protected:
+	UPROPERTY(BlueprintReadWrite, Replicated, Category = Pickup)
+	FText Name;
+
+	UPROPERTY(BlueprintReadWrite, Replicated, Category = Pickup)
+	bool bShowName;
+
+	UPROPERTY(BlueprintReadWrite, Replicated, Category = Pickup)
+	bool bIsStackable;
+
+	UPROPERTY(BlueprintReadWrite, Replicated, Category = Pickup)
+	int32 MaxStackSize;
+
+	UPROPERTY(BlueprintReadWrite, Replicated, Category = Pickup)
+	UTexture2D* InventoryImage;
+};
